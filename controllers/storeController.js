7fc4026bf003a088cb1fe1decc4886976c40a7bc
Artem Rosnovskiy <artem@rosnovsky.us>
2018-03-22 15:30:54 -0700

    Image uploads complete

--- controllers/storeController.js
+++ controllers/storeController.js
@@ -1,12 +1,52 @@
 const mongoose = require("mongoose");
 const Store = mongoose.model("Store");
+const multer = require("multer");
+const jimp = require("jimp");
+const uuid = require("uuid");
+
+const multerOptions = {
+	storage: multer.memoryStorage(),
+	fileFilter(req, file, next) {
+		const isPhoto = file.mimetype.startsWith("image");
+		if (isPhoto) {
+			next(null, true);
+		} else {
+			next(
+				{ message: "This file is not a picture. Please upload a picture." },
+				false
+			);
+		}
+	}
+};
 
 exports.homePage = (req, res) => {
 	res.render("index");
 };
 
+exports.showStore = async (req, res) => {
+	const store = await Store.findOne({ slug: req.params.slug });
+	if (!store) return next();
+	res.render("store", { title: store.name, store });
+};
+
 exports.addStore = (req, res) => {
 	res.render("editStore", { title: "Add Store" });
+};
+
+exports.upload = multer(multerOptions).single("photo");
+
+exports.resize = async (req, res, next) => {
+	if (!req.file) {
+		next();
+		return;
+	}
+
+	const extension = req.file.mimetype.split("/")[1];
+	req.body.photo = `${uuid.v4()}.${extension}`;
+	const photo = await jimp.read(req.file.buffer);
+	await photo.resize(800, jimp.AUTO);
+	await photo.write(`./public/uploads/${req.body.photo}`);
+	next();
 };
 
 exports.createStore = async (req, res) => {
--- models/Store.js
+++ models/Store.js
@@ -33,7 +33,8 @@ const storeSchema = new mongoose.Schema({
 			type: String,
 			required: "Address is required."
 		}
-	}
+	},
+	photo: String
 });
 
 storeSchema.pre("save", function(next) {
