109df5c11fc6b605b6b6ad1e4b4120e7fc5e9bb2
Artem Rosnovskiy <artem@rosnovsky.us>
2018-03-25 22:48:34 -0700

    Password reset flow complete

--- routes/index.js
+++ routes/index.js
@@ -43,4 +43,20 @@ router.post(
 
 router.get("/logout", authController.logout);
 
+router.get("/account", authController.isLoggedIn, userController.account);
+router.post(
+	"/account",
+	authController.isLoggedIn,
+	catchErrors(userController.updateAccount)
+);
+
+router.post("/account/forgot", catchErrors(authController.forgot));
+
+router.get("/account/reset/:token", catchErrors(authController.reset));
+router.post(
+	"/account/reset/:token",
+	authController.confirmPasswords,
+	catchErrors(authController.updatePassword)
+);
+
 module.exports = router;
